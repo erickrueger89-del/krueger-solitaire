0afe56a99189c73a651e21041476d4ce1aef5998
erickrueger89-del <312143043+erickrueger89-del@users.noreply.github.com>
2026-08-02 18:17:25 -0500

    Build Cards module

--- js/cards.js
+++ js/cards.js
@@ -1,31 +1,33 @@
 window.KID = window.KID || {};
 
 window.KID.Cards = {
-  suits: ["S", "H", "D", "C"],
+    suits: ["♠", "♥", "♦", "♣"],
+    ranks: [
+        "A","2","3","4","5","6","7",
+        "8","9","10","J","Q","K"
+    ],
 
-  ranks: [
-    "A", "2", "3", "4", "5", "6", "7",
-    "8", "9", "10", "J", "Q", "K"
-  ],
+    createDeck() {
+        const deck = [];
 
-  createDeck() {
-    const deck = [];
-
-    this.suits.forEach((suit) => {
-      this.ranks.forEach((rank) => {
-        deck.push({
-          suit,
-          rank
+        this.suits.forEach(suit => {
+            this.ranks.forEach(rank => {
+                deck.push({
+                    suit,
+                    rank,
+                    color:
+                        (suit === "♥" || suit === "♦")
+                            ? "red"
+                            : "black"
+                });
+            });
         });
-      });
-    });
 
-    return deck;
-  }
+        return deck;
+    }
 };
 
 console.log(
-  "Cards module loaded:",
-  window.KID.Cards.createDeck().length,
-  "cards"
+    "Cards loaded:",
+    window.KID.Cards.createDeck().length
 );
