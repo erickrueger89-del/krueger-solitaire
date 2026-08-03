cd90c528dc9b466ba9e64d8164f89ece0b8242be
erickrueger89-del <312143043+erickrueger89-del@users.noreply.github.com>
2026-08-03 08:54:00 -0500

    Validate shuffled deck

--- js/shuffle.js
+++ js/shuffle.js
@@ -1,23 +1,22 @@
 window.KID = window.KID || {};
 
 window.KID.Shuffle = {
-    shuffle(deck) {
-        const cards = [...deck];
-
-        for (let i = cards.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [cards[i], cards[j]] = [cards[j], cards[i]];
-        }
-
-        return cards;
+  shuffle(deck) {
+    if (!window.KID.Cards.validateDeck(deck)) {
+      throw new Error("Cannot shuffle an invalid deck.");
     }
+
+    const cards = [...deck];
+
+    for (let i = cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [cards[i], cards[j]] = [cards[j], cards[i]];
+    }
+
+    if (!window.KID.Cards.validateDeck(cards)) {
+      throw new Error("Shuffled deck validation failed.");
+    }
+
+    return cards;
+  }
 };
-
-const deck = window.KID.Cards.createDeck();
-const shuffled = window.KID.Shuffle.shuffle(deck);
-
-console.log(
-    "Shuffled deck:",
-    shuffled.length,
-    "cards"
-);
