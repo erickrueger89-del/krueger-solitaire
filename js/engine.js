64eff497c78545a30ea9a519b501b8a13c57d61f
erickrueger89-del <312143043+erickrueger89-del@users.noreply.github.com>
2026-08-03 09:05:51 -0500

    Centralize KID game-state creation

--- js/engine.js
+++ js/engine.js
@@ -4,39 +4,56 @@ window.KID.version = "0.3.1";
 
 window.KID.Engine = {
   state: null,
+  lastRender: null,
+
+  createState() {
+    const deck = window.KID.Cards.createDeck();
+
+    if (!window.KID.Cards.validateDeck(deck)) {
+      throw new Error("KID deck validation failed.");
+    }
+
+    const shuffledDeck = window.KID.Shuffle.shuffle(deck);
+    const state = window.KID.Deal.create(shuffledDeck);
+
+    if (!window.KID.Deal.validateState(state)) {
+      throw new Error("KID game-state validation failed.");
+    }
+
+    return state;
+  },
 
   start() {
-    const deck = window.KID.Cards.createDeck();
-    if (!window.KID.Cards.validateDeck(deck)) {
-    throw new Error("KID deck validation failed.");
-}
-    const shuffledDeck = window.KID.Shuffle.shuffle(deck);
+    this.state = this.createState();
+    this.lastRender = window.KID.Render.draw(this.state);
 
-    this.state = window.KID.Deal.create(shuffledDeck);
-window.KID.Render.draw(this.state);
+    if (!this.lastRender) {
+      throw new Error("KID render failed during startup.");
+    }
+
     console.log("KID Engine started.");
     console.log(
-        "Tableau columns:",
-        this.state.tableau.length,
-        "Stock cards:",
-        this.state.stock.length
+      "Tableau columns:",
+      this.state.tableau.length,
+      "Stock cards:",
+      this.state.stock.length
     );
-},
 
-newGame() {
-    const deck =  window.KID.Cards.createDeck();
-      if (!window.KID.Cards.validateDeck(deck)) {
-  throw new Error("KID deck validation failed.");
-}
-    const shuffledDeck = window.KID.Shuffle.shuffle(deck);
-
-    this.state = window.KID.Deal.create(shuffledDeck);
-window.KID.Render.draw(this.state);
     return this.state;
-    },
-    
-about() {
-    
+  },
+
+  newGame() {
+    this.state = this.createState();
+    this.lastRender = window.KID.Render.draw(this.state);
+
+    if (!this.lastRender) {
+      throw new Error("KID render failed during new game.");
+    }
+
+    return this.state;
+  },
+
+  about() {
     return {
       name: "Krueger Solitaire",
       developer: "Krueger Interactive Developments",
