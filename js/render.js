e315ef8fa4d4c406d601a4e554eef57c1cba0a83
erickrueger89-del <312143043+erickrueger89-del@users.noreply.github.com>
2026-08-03 08:19:22 -0500

    Create Render module

---
+++ js/render.js
@@ -0,0 +1,11 @@
+window.KID = window.KID || {};
+
+window.KID.Render = {
+    draw(state) {
+        console.log(
+            "Rendering",
+            state.tableau.length,
+            "tableau columns."
+        );
+    }
+};
