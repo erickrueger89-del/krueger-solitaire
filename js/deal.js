bf3482d2afe6c2d9343dd3b6326a2310f5820bdc
erickrueger89-del <312143043+erickrueger89-del@users.noreply.github.com>
2026-08-03 06:39:35 -0500

    Create Deal module

---
+++ js/deal.js
@@ -0,0 +1,10 @@
+window.KID = window.KID || {};
+
+window.KID.Deal = {
+    deal(deck) {
+        console.log("Deal module ready.");
+        return deck;
+    }
+};
+
+console.log("Deal module loaded.");
